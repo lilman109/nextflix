fde6482b09059518716ca3484f100f77ff1cf462
lilman109 <akira.yoshidasjsu@gmail.com>
2023-02-08 20:28:37 +0900

    use Roboto_Slab as font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Roboto_Slab } from "@next/font/google";
+
+const roboto = Roboto_Slab({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
