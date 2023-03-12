87044b39febdc9c9b1211028dcea3a994364531f
lilman109 <akira.yoshidasjsu@gmail.com>
2023-03-13 01:28:17 +0900

    wip: added banner

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 
+import Banner from "@/components/banner/banner";
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -8,7 +10,14 @@ export default function Home() {
         <title>Nextflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <h1>Nextflix</h1>
+
+      <Banner
+        title="Clifford the Red Dog"
+        subTitle="a very cute dog"
+        imgUrl="/static/clifford.webp"
+      />
     </div>
   );
 }
