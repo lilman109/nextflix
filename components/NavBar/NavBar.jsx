9d432e89f79aeb6e2d7be9a2ba25f7a45937963d
lilman109 <akira.yoshidasjsu@gmail.com>
2023-03-16 00:48:59 +0900

    added router and link for items and signout

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,32 +1,53 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import styles from "./NavBar.module.css";
 
 const NavBar = (props) => {
   const { userName } = props;
+
+  const router = useRouter();
+
+  const handleOnClickHome = () => {
+    e.preventDefault();
+    router.push("/");
+  };
+
+  const handleOnClickMyList = (e) => {
+    e.preventDefault();
+    router.push("/browse/my-list");
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <a className={styles.logoLink} href="/">
           <div className={styles.logoWrapper}>NextFlix</div>
         </a>
-      <ul className={styles.navItems}>
-        <li className={styles.navItem}>Home</li>
-        <li className={styles.navItem2}>My List</li>
-      </ul>
-      <nav className={styles.navContainer}>
-        <div>
-          <button className={styles.usernameBtn}>
-            <p className={styles.username}>{userName}</p>
-            {/** Expand more icon */}
-          </button>
-        </div>
-        <div className={styles.navDropdown}>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem} onClick={handleOnClickHome}>
+            Home
+          </li>
+          <li className={styles.navItem2} onClick={handleOnClickMyList}>
+            My List
+          </li>
+        </ul>
+        <nav className={styles.navContainer}>
           <div>
-            <a className={styles.linkName}>Sign Out</a>
-            <div className={styles.lineWrapper} />
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{userName}</p>
+              {/** Expand more icon */}
+            </button>
           </div>
-        </div>
-      </nav>
+          <div className={styles.navDropdown}>
+            <div>
+              <Link className={styles.linkName} href="/login">
+                Sign Out
+              </Link>
+              <div className={styles.lineWrapper} />
+            </div>
+          </div>
+        </nav>
       </div>
     </div>
   );
