afe77455417094a2092513d26392217b44142a76
lilman109 <akira.yoshidasjsu@gmail.com>
2023-03-16 00:35:20 +0900

    navbar styling

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,24 +1,33 @@
 import React from "react";
+import styles from "./NavBar.module.css";
 
 const NavBar = (props) => {
   const { userName } = props;
   return (
-    <div>
-      <p>{userName}</p>
-      <ul>
-        <li>Home</li>
-        <li>My List</li>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}>NextFlix</div>
+        </a>
+      <ul className={styles.navItems}>
+        <li className={styles.navItem}>Home</li>
+        <li className={styles.navItem2}>My List</li>
       </ul>
-      <nav>
+      <nav className={styles.navContainer}>
         <div>
-          <button>
-            <p>{userName}</p>
+          <button className={styles.usernameBtn}>
+            <p className={styles.username}>{userName}</p>
+            {/** Expand more icon */}
           </button>
         </div>
-        <div>
-          <a>Sign Out</a>
+        <div className={styles.navDropdown}>
+          <div>
+            <a className={styles.linkName}>Sign Out</a>
+            <div className={styles.lineWrapper} />
+          </div>
         </div>
       </nav>
+      </div>
     </div>
   );
 };
